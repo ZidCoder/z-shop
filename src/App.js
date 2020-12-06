5636672c4aeb9a930a94fd9dd0812000cbb72f2f
zidcoder <ozidhalawa97@gmail.com>
2020-12-06 11:02:23 +0700

    testing git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import react from 'react'
+import Button from 'elements/Button/index'
+import 'assets/scss/style.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Button isLarge isPrimary >Get started</Button>
+    </div>
+  );
+}
+
+export default App;
